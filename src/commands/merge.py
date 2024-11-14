980192f242dba7b38ab7cf788038a2433f2ce860
Idan Stark <idanstark42@gmail.com>
2024-11-14 17:02:24 +0200

    better merge?

--- src/commands/merge.py
+++ src/commands/merge.py
@@ -5,85 +5,51 @@ import numpy as np
 
 from settings import ETA_RANGE
 
+FIELDS = ['event', 'tracks', 'clusters', 'truthTaus']
+
 def create_output_file (output_file, input_file):
   print('Loading input file...')
   with h5py.File(input_file, 'r') as input:
-    event = input['event'][:]
-    tracks = input['tracks'][:]
-    clusters = input['clusters'][:]
-    truthTaus = input['truthTaus'][:]
-
-    event, tracks, clusters, truthTaus = clean(event, tracks, clusters, truthTaus)
-  
     print('Creating output file...')
     with h5py.File(output_file, 'w') as output:
-      print('Creating event dataset...')
-      output.create_dataset(
-        "event",
-        data=event,
-        compression="gzip",
-        chunks=(1,),
-        maxshape=(None,),
-      )
-      print('Creating tracks dataset...')
-      output.create_dataset(
-        "tracks",
-        data=tracks,
-        compression="gzip",
-        chunks=(1, tracks.shape[1]),
-        maxshape=(None, tracks.shape[1]),
-      )
-      print('Creating clusters dataset...')
-      output.create_dataset(
-        "clusters",
-        data=clusters,
-        compression="gzip",
-        chunks=(1, clusters.shape[1]),
-        maxshape=(None, clusters.shape[1]),
-      )
-      print('Creating truthTaus dataset...')
-      output.create_dataset(
-        "truthTaus",
-        data=truthTaus,
-        compression="gzip",
-        chunks=(1, truthTaus.shape[1]),
-        maxshape=(None, truthTaus.shape[1]),
-      )
+      invalid_indices = get_invalid_indices(input['truthTaus'][:])
+      print(f'Dropping {len(invalid_indices)} invalid events ({len(invalid_indices)/len(input["event"])*100:.2f}%)')
+
+      for field in FIELDS:
+        print(f'Creating {field} dataset...')
+        data = np.delete(input[field][:], invalid_indices, axis=0)
+        output.create_dataset(
+          field,
+          data=data,
+          compression="gzip",
+          chunks=(1, input[field].shape[1]) if len(input[field].shape) > 1 else (1,),
+          maxshape=(None, input[field].shape[1]) if len(input[field].shape) > 1 else (None,),
+        )
+        del data
   print('Done')
 
 def append_to_output_file (output_file, input_file):
+  print('Loading input file...')
   with h5py.File(input_file, 'r') as input:
-    event = input['event'][:]
-    tracks = input['tracks'][:]
-    clusters = input['clusters'][:]
-    truthTaus = input['truthTaus'][:]
-
-    event, tracks, clusters, truthTaus = clean(event, tracks, clusters, truthTaus)
-
+    print('Loading output file...')
     with h5py.File(output_file, 'a') as output:
-      output['event'].resize((output['event'].shape[0] + event.shape[0]), axis=0)
-      output['event'][-event.shape[0]:] = event
-      output['tracks'].resize((output['tracks'].shape[0] + tracks.shape[0]), axis=0)
-      output['tracks'][-tracks.shape[0]:] = tracks
-      output['clusters'].resize((output['clusters'].shape[0] + clusters.shape[0]), axis=0)
-      output['clusters'][-clusters.shape[0]:] = clusters
-      output['truthTaus'].resize((output['truthTaus'].shape[0] + truthTaus.shape[0]), axis=0)
-      output['truthTaus'][-truthTaus.shape[0]:] = truthTaus
+      invalid_indices = get_invalid_indices(input['truthTaus'][:])
+      print(f'Dropping {len(invalid_indices)} invalid events ({len(invalid_indices)/len(input["event"])*100:.2f}%)')
 
-def clean(event, tracks, clusters, truthTaus):
-  #Find invalid events (based on ==2 truthTaus with |eta| < 2.5)
+      for field in FIELDS:
+        print(f'Appending {field} dataset...')
+        data = np.delete(input[field][:], invalid_indices, axis=0)
+        output[field].resize((output[field].shape[0] + data.shape[0]), axis=0)
+        output[field][-data.shape[0]:] = data
+        del data
+  print('Done')
+
+def get_invalid_indices(truthTaus):
   truthTaus_expanded = np.array(truthTaus.tolist())
   num_truthtaus = np.sum(~np.isnan(truthTaus_expanded[:,:,0:truthTaus.shape[1]]), axis=1)
   not_two_truthtaus = np.unique(np.where(num_truthtaus != 2)[0])
   not_two_barrel_Taus = np.unique(np.where(np.abs(truthTaus_expanded[:, :2, 1]) > ETA_RANGE[1])[0])
-  invalid_indices = np.unique(np.concatenate((not_two_truthtaus, not_two_barrel_Taus)))
-  print(f'Dropping {len(invalid_indices)} invalid events ({len(invalid_indices)/len(event)*100:.2f}%)')
-  #Drop invalid events
-  event = np.delete(event, invalid_indices, axis=0)
-  tracks = np.delete(tracks, invalid_indices, axis=0)
-  clusters = np.delete(clusters, invalid_indices, axis=0)
-  truthTaus = np.delete(truthTaus, invalid_indices, axis=0)
-  return event, tracks, clusters, truthTaus
+  return np.unique(np.concatenate((not_two_truthtaus, not_two_barrel_Taus)))
 
 def merge (input_files, output_file, create_output=True):
   print(f'Merging {len(input_files)} files into {output_file}')
