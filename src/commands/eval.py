8001cec538206fa71775cab48b703c414cf618e2
Idan Stark <idanstark42@gmail.com>
2024-11-19 11:13:40 +0200

    bugfix

--- src/commands/eval.py
+++ src/commands/eval.py
@@ -8,12 +8,15 @@ def evaluate (dataset, module, model_file, output_folder, params):
   random_indices = torch.randperm(len(dataset))[:n]
   module.eval()
   print()
-  print('Calculating outputs...')
   with torch.no_grad():
+    print('1. Preparing data')
+    events = [dataset.get_event(i) for i in random_indices]
     inputs, targets = zip(*[dataset[i] for i in random_indices])
     inputs = torch.stack(inputs)
     targets = torch.stack(targets)
+    print('2. Running model')
     outputs = module(inputs)
-
+    print('3. Visualizing results')
     visualizer = ModelVisualizer(module)
-    visualizer.show_reconstruction_rate_stats(outputs, targets, dataset, output_folder)
+    visualizer.show_reconstruction_rate_stats(outputs, targets, events, output_folder)
+    print('4. Done')
