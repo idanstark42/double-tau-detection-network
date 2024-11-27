4112f1cf0bd834ef2c198aa342c2f393423983da
Idan Stark <idanstark42@gmail.com>
2024-11-27 20:07:55 +0200

    trying this a different way

--- src/commands/tune.py
+++ src/commands/tune.py
@@ -1,24 +1,44 @@
-from ray import tune, init
+import numpy as np
 import os
+from math import Infinity
 
 from commands.train import train
 
 def tune_hyperparameters (dataset, model, model_folder, options):
-  # Define the search space
   search_space = {
-    "learning_rate": tune.loguniform(1e-4, 1e-1),
-    "weight_decay": tune.loguniform(1e-6, 1e-3),
-    "batch_size": tune.choice([32, 64, 128, 256, 512]),
+    "learning_rate": np.logspace(-5, -1, num=10),
+    "weight_decay": np.logspace(-5, -1, num=10),
+    "batch_size": np.array([32, 64, 128, 256, 512])
   }
+
+  print("Tuning hyperparameters.")
+  print("Search space:")
+  for key, values in search_space.items():
+    print(f"  {key}: {values}")
 
   # Define the training function
   def train_model (config):
     submodel_folder = os.path.join(model_folder, '-'.join([f"{key}({value})" for key, value in config.items()]))
     return train(dataset, model, submodel_folder, { **options, **config, })
+  
+  print("Creating grid search space.")
+  grid = np.array(np.meshgrid(*search_space.values())).T.reshape(-1, len(search_space))
 
-  init(local_mode=True)
-  # Start the hyperparameter search
-  analysis = tune.run(train_model, config=search_space)
+  best_loss = Infinity
+  best_config = None
+  losses = []
+  print("Starting grid search.")
+  for config in grid:
+    loss = (config, train_model(config))
+    losses.append((loss, config))
+    if loss < best_loss:
+      best_loss = loss
+      best_config = config
 
-  # Return the best hyperparameters
-  return analysis.get_best_config(metric="loss", mode="min")
+  # print the best configuration
+  print('Done.')
+  print()
+  print(f"Best configuration (loss: {best_loss}):")
+  for key, value in best_config.items():
+    print(f"  {key}: {value}")
+    
